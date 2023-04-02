e8c0b0c0bb277d314ae9bf4e10507754c11be08c
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-02 15:58:26 +0100

    Fixing projects so its working

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// "use client";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex font-abc">
+      <h1>Vitalina's chat</h1>
+    </div>
+  );
+}
